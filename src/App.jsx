4cd1bbf3b563955116a48ef5eb6c800546b86f8a
dilbot-cot <131760791+dilbot-cot@users.noreply.github.com>
2024-05-30 14:42:27 +1000

    fix: removed legacy imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { Route, Routes } from 'react-router-dom'
-import Footer from './components/Footer'
-import NavBar from './components/NavBar'
-import GithubPage from './pages/GithubPage'
 import Template from './pages/_TemplatePage'
 import GitHubRepos from './pages/GithubPage'
 import HomePage from './pages/HomePage'
